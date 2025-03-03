9bb8882fb72871eceb6fb336dcb1d1d02d64ce34
telbip <telbipmy@proton.me>
2025-03-03 20:42:49 +0200

    init

---
+++ data/config.py
@@ -0,0 +1,3 @@
+# Captcha service settings
+CAPTCHA_SERVICE = "2captcha"  # Captcha solving service (available: 2captcha, capmonster, anticaptcha)
+CAPTCHA_API_KEY = "api"  # API key for the service
